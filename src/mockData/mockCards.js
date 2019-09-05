d78dbd106593af5ace7857369bf5a8b5fc9ab3c7
Turing School <kratok08@gmail.com>
2019-09-05 17:05:17 -0600

    Update mock data

--- src/mockData/mockCards.js
+++ src/mockData/mockCards.js
@@ -7,20 +7,50 @@ export const cards = [
     spendingPower: 0,
     actionsProvided: 2,
     cardsToDraw: 1,
-    image: "../assets/card_images/Village.jpg",
+    image: "Village.jpg",
     id: 123123
   },
   {
     name: "Gold",
     type: "Treasure",
     cost: 6,
+    image: "Gold.jpg",
     spendingPower: 3
+  },
+  {
+    name: "Silver",
+    type: "Treasure",
+    cost: 3,
+    image: "Silver.jpg",
+    spendingPower: 2
+  },
+  {
+    name: "Copper",
+    type: "Treasure",
+    cost: 0,
+    image: "Copper.jpg",
+    spendingPower: 1
+  },
+  {
+    name: "Estate",
+    type: "Victory",
+    cost: 2,
+    image: "Estate.jpg",
+    victoryPoints: 1
   },
   {
     name: "Duchy",
     type: "Victory",
     cost: 5,
+    image: "Duchy.jpg",
     victoryPoints: 3
+  },
+  {
+    name: "Province",
+    type: "Victory",
+    cost: 8,
+    image: "Province.jpg",
+    victoryPoints: 6
   }
 ];
 
