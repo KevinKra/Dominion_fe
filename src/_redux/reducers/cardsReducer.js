352392dcdf6748a44859c4350dd17c247b2af111
Turing School <kratok08@gmail.com>
2019-09-05 14:12:35 -0600

    Rename reducer to tableCards

--- src/_redux/reducers/cardsReducer.js
+++
@@ -1,8 +0,0 @@
-export const cardsReducer = (state = [], action) => {
-  switch (action.type) {
-    case "UPDATE_CURRENT_CARDS":
-      return action.cards;
-    default:
-      return state;
-  }
-};
