95206f332098cf89fbd13827ee948e0e5c63ec64
Turing School <kratok08@gmail.com>
2019-09-05 18:24:01 -0600

    Create loading conditional and map new tag props

--- src/_components/Card/Card.js
+++ src/_components/Card/Card.js
@@ -1,61 +1,71 @@
 import React from "react";
-import images from "../../assets/card_images/Village.jpg";
 import "./Card.scss";
 
-export default function Card({ name, desc, type, image, cost }) {
+export default function Card({ name, desc, type, image, cost, tags }) {
   const handleRender = () => {
-    if (type === "Action") {
-      return (
-        <article className='card'>
-          <header style={{ backgroundColor: "silver" }}>{name}</header>
-          {image ? (
-            <img src={require(`../../assets/card_images/${image}`)} alt='' />
-          ) : null}
-          <p className='description'>{desc}</p>
-          <footer>
-            <p>{cost}</p>
-            <p>{type}</p>
-          </footer>
-        </article>
-      );
-    } else if (type === "Treasure") {
-      return (
-        <article className='card'>
-          <header style={{ position: "relative", backgroundColor: "gold" }}>
-            {name}
-          </header>
-          {image ? (
-            <img
-              src={require(`../../assets/card_images/${image}`)}
-              alt={name}
-              style={{ flex: 1 }}
-            />
-          ) : null}
-          <footer>
-            <p>{cost}</p>
-            <p>{type}</p>
-          </footer>
-        </article>
-      );
+    if (type !== undefined) {
+      if (type[0] === "Action") {
+        return (
+          <article className='card'>
+            <header style={{ backgroundColor: "silver" }}>{name}</header>
+            {image ? (
+              <img src={require(`../../assets/card_images/${image}`)} alt='' />
+            ) : null}
+            <div className='card-details'>
+              {tags.map((tag, i) => (
+                <p className='tag' key={i}>
+                  {tag}
+                </p>
+              ))}
+              <p className='description'>{desc}</p>
+            </div>
+            <footer>
+              <p>{cost}</p>
+              <p>{type}</p>
+            </footer>
+          </article>
+        );
+      } else if (type[0] === "Treasure") {
+        return (
+          <article className='card'>
+            <header style={{ position: "relative", backgroundColor: "gold" }}>
+              {name}
+            </header>
+            {image ? (
+              <img
+                src={require(`../../assets/card_images/${image}`)}
+                alt={name}
+                style={{ flex: 1 }}
+              />
+            ) : null}
+            <footer>
+              <p>{cost}</p>
+              <p>{type}</p>
+            </footer>
+          </article>
+        );
+      } else {
+        return (
+          <article className='card'>
+            <header style={{ position: "relative", backgroundColor: "lightGreen" }}>
+              {name}
+            </header>
+            {image ? (
+              <img
+                src={require(`../../assets/card_images/${image}`)}
+                alt={name}
+                style={{ flex: 1 }}
+              />
+            ) : null}
+            <footer>
+              <p>{cost}</p>
+              <p>{type}</p>
+            </footer>
+          </article>
+        );
+      }
     } else {
-      return (
-        <article className='card'>
-          <header style={{ position: "relative", backgroundColor: "lightGreen" }}>
-            {name}
-          </header>
-          {image ? (
-            <img
-              src={require(`../../assets/card_images/${image}`)}
-              alt={name}
-              style={{ flex: 1 }}
-            />
-          ) : null}
-          <footer>
-            <p>{cost}</p>
-            <p>{type}</p>
-          </footer>
-        </article>
-      );
+      return <p>Loading</p>;
     }
   };
 
