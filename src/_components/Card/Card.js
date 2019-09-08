961aa95b0c792e503a6c496125e5afbd775e860b
Turing School <kratok08@gmail.com>
2019-09-07 19:52:24 -0600

    Wrap card details jsx div with section

--- src/_components/Card/Card.js
+++ src/_components/Card/Card.js
@@ -11,14 +11,16 @@ export default function Card({ name, desc, category, image, cost, tags }) {
             {image ? (
               <img src={require(`../../assets/card_images/${image}`)} alt='' />
             ) : null}
-            <div className='card-details'>
-              {tags.map((tag, i) => (
-                <p className='tag' key={i}>
-                  {tag}
-                </p>
-              ))}
-              <p className='description'>{desc}</p>
-            </div>
+            <section className='card-details-section'>
+              <div className='card-details'>
+                {tags.map((tag, i) => (
+                  <p className='tag' key={i}>
+                    {tag}
+                  </p>
+                ))}
+                <p className='description'>{desc}</p>
+              </div>
+            </section>
             <footer>
               <p>{cost}</p>
               <p>{category}</p>
