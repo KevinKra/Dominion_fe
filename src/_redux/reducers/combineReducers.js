27a01ab265485887f8e89e52bc494a77f3fcb7dc
Vinton Teo <taulago@ymail.com>
2019-09-08 01:37:43 -0600

    Add playerTurn to the rootReducer.

--- src/_redux/reducers/combineReducers.js
+++ src/_redux/reducers/combineReducers.js
@@ -3,9 +3,11 @@ import { tableCards } from "./tableCards";
 import { playerDeck } from "./playerDeck";
 import { playerHand } from "./playerHand";
 import { discardPile } from "./discardPile";
+import { playerTurn } from "./playerTurn";
 
 export const rootReducer = combineReducers({
   tableCards,
+  playerTurn,
   playerDeck,
   playerHand,
   discardPile
