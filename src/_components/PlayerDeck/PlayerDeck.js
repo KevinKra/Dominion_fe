2f70fd5ce82e3a90fd826924e779fffcda9271c1
Turing School <kratok08@gmail.com>
2019-09-09 14:19:26 -0600

    Add mock data and update styles for base media support

--- src/_components/PlayerDeck/PlayerDeck.js
+++ src/_components/PlayerDeck/PlayerDeck.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./PlayerDeck.scss";
 import Card from "../Card/Card";
+import { mockTreasureCard } from "../../mockData";
 
 export default function PlayerDeck() {
   return (
     <section className='PlayerDeck'>
-      <Card />
+      <Card
+        name={mockTreasureCard.name}
+        desc={mockTreasureCard.desc}
+        category={mockTreasureCard.category[0]}
+        image={mockTreasureCard.image}
+        cost={mockTreasureCard.cost}
+        id={mockTreasureCard.id}
+        key={mockTreasureCard.id}
+      />
     </section>
   );
 }
