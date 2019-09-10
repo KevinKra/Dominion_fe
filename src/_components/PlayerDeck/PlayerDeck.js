893cca19448e4eca55ced5cff5eea3c37c587cae
Vinton Teo <taulago@ymail.com>
2019-09-10 00:48:57 -0600

    Convert PlayerDeck to class component.

--- src/_components/PlayerDeck/PlayerDeck.js
+++ src/_components/PlayerDeck/PlayerDeck.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import { Connect } from "react-redux";
+import * as actions from "../../_redux/actions";
 import "./PlayerDeck.scss";
 import CardBack from "../CardBack/CardBack";
 
-export default function PlayerDeck() {
+export class PlayerDeck extends Component {
+
+  render() {
   return (
-    <section className='PlayerDeck'>
+      <section
+        className="PlayerDeck"
+        onClick={drawCount => this.draw(drawCount)}
+      >
       <CardBack />
     </section>
   );
 }
+}
+
+const mapStateToProps = state => ({
+  deck: state.playerDeck,
+  hand: state.playerHand,
+  discard: state.discardPile
+});
+
+const mapDispatchToProps = dispatch => ({
+  updatePlayerCards: (deck, hand, discard) =>
+    dispatch(actions.updatePlayerCards(deck, hand, discard))
+});
+
+export default Connect(mapStateToProps)(PlayerDeck);
