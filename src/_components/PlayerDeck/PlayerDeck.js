b6e09ba83ab40ff6adfb509e012aee5bcfc4f335
Kevin Krato <43081008+KevinKra@users.noreply.github.com>
2019-09-09 19:59:26 -0600

    Feature/add discard pile; apply mock data; remove business logic; media query style
    * Change mock cards key type to category to avoid reserved keyword issue. Update components accordingly.
    
    * Refactor components to remove business logic from view
    
    * Update snapshots
    
    * Add additional actions for updating reducers
    
    * Create mock data for each card variant
    
    * Create and update snapshots for all components
    
    * Add basic media query parameters
    
    * Create styles dir
    
    * Wrap card details jsx div with section
    
    * Update styles
    
    * Normalize css
    
    * Update metadata
    
    * Add mock data and update styles for base media support
    
    * Update snapshots
    
    * Create CardBack component
    
    * Create DiscardPile component
    
    * Add cardBack image to assets dir
    
    * Update components with new card data
    
    * Update styles
    
    * Update snapshots

--- src/_components/PlayerDeck/PlayerDeck.js
+++ src/_components/PlayerDeck/PlayerDeck.js
@@ -1,20 +1,11 @@
 import React from "react";
 import "./PlayerDeck.scss";
-import Card from "../Card/Card";
-import { mockTreasureCard } from "../../mockData";
+import CardBack from "../CardBack/CardBack";
 
 export default function PlayerDeck() {
   return (
     <section className='PlayerDeck'>
-      <Card
-        name={mockTreasureCard.name}
-        desc={mockTreasureCard.desc}
-        category={mockTreasureCard.category[0]}
-        image={mockTreasureCard.image}
-        cost={mockTreasureCard.cost}
-        id={mockTreasureCard.id}
-        key={mockTreasureCard.id}
-      />
+      <CardBack />
     </section>
   );
 }
