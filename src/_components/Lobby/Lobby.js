1d512b12b949ecc0b4cb2656e0e0725b9fad1271
Turing School <kratok08@gmail.com>
2019-09-10 17:37:11 -0600

    Add fetch functionality to handlesubmit

--- src/_components/Lobby/Lobby.js
+++ src/_components/Lobby/Lobby.js
@@ -10,18 +10,36 @@ export default class Lobby extends Component {
     this.setState({ username: value });
   };
 
-  handleSubmit = () => {
-    //should create a game object with the user's name
-    //should update the game store object with the id of the current game
-
-    //should route to new page
+  handleSubmit = async e => {
+    e.preventDefault();
+    // const response = await fetch("http://localhost:3000/api/v1/games/1/players/1");
+    // console.log(response.json());
+    try {
+      const config = {
+        method: "POST",
+        // headers: {
+        //   Accept: "application/json",
+        //   "Content-Type": "application/json"
+        // },
+        body: JSON.stringify({ newPlayer: { name: this.state.username } })
+      };
+      const response = await fetch("http://localhost:3000/api/v1/games", config);
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log(config.body);
+        console.log("hello", response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
     this.props.history.push("/current-game");
   };
 
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={e => this.handleSubmit(e)}>
           <label htmlFor='username'>Username:</label>
           <input type='text' name='username' onChange={e => this.handleChange(e)} />
           <button>START GAME</button>
