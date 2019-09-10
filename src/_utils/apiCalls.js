215ed65c46232bc92ee13193e480e6f3768ec80b
Vinton Teo <taulago@ymail.com>
2019-09-10 12:10:47 -0600

    Create joinLobby function to link player to instance of the game.

---
+++ src/_utils/apiCalls.js
@@ -0,0 +1,19 @@
+export const joinLobby = async username => {
+  const url = "http://localhost:3000";
+  const path = "/api/v1/games";
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ newPlayer: { name: username } })
+  };
+  try {
+    const response = await fetch(url + path, options);
+    if (!response.ok) {
+      throw new Error("Failed to join lobby.");
+    }
+    const playerToken = await response.json();
+    return playerToken;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
