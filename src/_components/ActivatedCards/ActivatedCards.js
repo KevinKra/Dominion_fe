c6d4501eb09f5433af467ec77b0e8e1b370907a0
Turing School <kratok08@gmail.com>
2019-09-11 16:28:13 -0600

    Build out component

--- src/_components/ActivatedCards/ActivatedCards.js
+++ src/_components/ActivatedCards/ActivatedCards.js
@@ -1,6 +1,30 @@
 import React from "react";
+import Card from "../Card/Card";
+import { connect } from "react-redux";
 import "./ActivatedCards.scss";
 
-export default function ActivatedCards() {
-  return <section className='ActivatedCards'>ActivatedCards</section>;
+export function ActivatedCards(props) {
+  return (
+    <section className='ActivatedCards'>
+      {props.activatedCards.map(card => {
+        return (
+          <Card
+            name={card.name}
+            desc={card.desc}
+            category={card.category}
+            image={card.image}
+            cost={card.cost}
+            id={card.id}
+            key={card.id}
+          />
+        );
+      })}
+    </section>
+  );
 }
+
+export const mapStateToProps = state => ({
+  activatedCards: state.activatedCards
+});
+
+export default connect(mapStateToProps)(ActivatedCards);
