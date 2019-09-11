495c1c3170b5884823f6a55ea3740c4e3ea15ff3
Vinton Teo <taulago@ymail.com>
2019-09-10 18:14:37 -0600

    Create updateGameState fetch call.

--- src/_utils/apiCalls.js
+++ src/_utils/apiCalls.js
@@ -18,3 +18,18 @@ export const createLobby = async username => {
     throw Error(error.message);
   }
 };
+
+export const updateGameState = async gameID => {
+  const url = "http://localhost:3000";
+  const path = `api/v1/game_state/${gameID}`;
+  try {
+    const response = await fetch(url + path);
+    if (!response.ok) {
+      throw new Error("Failed to update game state.");
+    }
+    const gameState = await response.json();
+    return gameState;
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
