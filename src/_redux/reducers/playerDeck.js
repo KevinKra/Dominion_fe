04a1e16b7f2e45686bdf0b5a684c13f7981a39f6
Turing School <kratok08@gmail.com>
2019-09-11 08:27:45 -0600

    Fix reducer switch errors

--- src/_redux/reducers/playerDeck.js
+++ src/_redux/reducers/playerDeck.js
@@ -1,6 +1,6 @@
 export const playerDeck = (state = [], action) => {
   switch (action.type) {
-    case "UPDATE_PLAYER_DECK":
+    case "UPDATE_PLAYER_CARDS":
       return action.playerDeck;
     default:
       return state;
--- src/_redux/reducers/playerHand.js
+++ src/_redux/reducers/playerHand.js
@@ -1,6 +1,6 @@
 export const playerHand = (state = [], action) => {
   switch (action.type) {
-    case "UPDATE_PLAYER_HAND":
+    case "UPDATE_PLAYER_CARDS":
       return action.playerHand;
     default:
       return state;
