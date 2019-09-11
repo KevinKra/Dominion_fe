7b9acdbd6becffd9be063ed317792b5de07092b1
Turing School <kratok08@gmail.com>
2019-09-11 08:27:13 -0600

    Update component to determine whether or not player data needs to be updated, should only happen once at turn start.

--- src/_components/PlayerSection/PlayerSection.js
+++ src/_components/PlayerSection/PlayerSection.js
@@ -5,10 +5,14 @@ import ActivatedCards from "../ActivatedCards/ActivatedCards";
 import PlayerDeck from "../PlayerDeck/PlayerDeck";
 import PlayerHand from "../PlayerHand/PlayerHand";
 import DiscardPile from "../DiscardPile/DiscardPile";
-import { updateGameState } from "../../_utils/apiCalls";
+import { updateGameState, updatePlayerState } from "../../_utils/apiCalls";
 import "./PlayerSection.scss";
 
 export class PlayerSection extends Component {
+  state = {
+    dataUpdated: false
+  };
+
   componentDidMount = () => {
     this.turnInterval = setInterval(() => this.requestPlayerTurn(), 15000);
     this.requestPlayerTurn();
@@ -25,11 +29,30 @@ export class PlayerSection extends Component {
     if (gameState.activePlayerId === this.props.playerID) {
       console.log("It's your turn, turnInterval turned off. Please complete your turn.");
       clearInterval(this.turnInterval);
+      this.updatePlayerData();
     } else {
       console.log("Not your turn, continuing the interval check the turn");
+      this.updatePlayerData();
     }
     console.log(gameState);
   };
+
+  updatePlayerData = async () => {
+    if (!this.state.dataUpdated) {
+      const playerData = await updatePlayerState(this.props.gameID, this.props.playerID);
+      console.log(playerData);
+      this.props.updatePlayerCards(playerData.deck, null, playerData.discardPile || null);
+    }
+    this.setState({ dataUpdated: true });
+  };
+
+  //!!! need to reset dataUpdated to false at the start of every turn so players will fetch
+  //their data once regardless whether they are active or waiting
+  startNewTurn() {
+    //this function needs to reset dataUpdatedToFalse
+    //needs to be called at the start of requestPlayerTurn to reset local state
+    //then both players are able to fetch their respective cards and toggle that state to off.
+  }
 
   render() {
     return (
