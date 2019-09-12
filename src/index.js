24c76238f924b0a4198449494792583136dd2f8a
Vinton Teo <taulago@ymail.com>
2019-09-11 19:09:23 -0600

    Delete logo, serviceWorker, and associated calls.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./_components/App/App";
 import { BrowserRouter } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 import "./normalize.css";
 
@@ -21,8 +20,3 @@ const application = (
 );
 
 ReactDOM.render(application, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
