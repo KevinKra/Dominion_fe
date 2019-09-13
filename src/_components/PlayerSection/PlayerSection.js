7ecee2597fc5d298200e1f0ebf144a788b522fea
Vinton Teo <taulago@ymail.com>
2019-09-13 12:02:31 -0600

    Fix shuffle logic to be called when deck length is less than five.

--- src/_components/PlayerSection/PlayerSection.js
+++ src/_components/PlayerSection/PlayerSection.js
@@ -14,7 +14,6 @@ export class PlayerSection extends Component {
   };
 
   componentDidMount = () => {
-    // this.turnInterval = setInterval(() => this.requestPlayerTurn(), 15000);
     this.startTimer();
     this.requestPlayerTurn();
   };
@@ -34,7 +33,9 @@ export class PlayerSection extends Component {
     }
     if (gameState.activePlayerId === this.props.playerID) {
       //active player
-      console.log("It's your turn, turnInterval turned off. Please complete your turn.");
+      console.log(
+        "It's your turn, turnInterval turned off. Please complete your turn."
+      );
       clearInterval(this.turnInterval);
       this.turnInterval = undefined;
 
@@ -65,7 +66,10 @@ export class PlayerSection extends Component {
 
   updatePlayerData = async () => {
     if (!this.state.dataUpdated) {
-      const playerData = await updatePlayerState(this.props.gameID, this.props.playerID);
+      const playerData = await updatePlayerState(
+        this.props.gameID,
+        this.props.playerID
+      );
       console.log(playerData);
       const drawnCards = this.draw(playerData.deck);
       console.log(drawnCards);
@@ -86,7 +90,7 @@ export class PlayerSection extends Component {
     );
     const discardedIds = this.props.discardPile.map(card => card.id);
 
-    if (!this.props.playerDeck.length) {
+    if (this.props.playerDeck.length < 5) {
       const newDeck = this.shuffle([
         ...boughtCardIds,
         ...handCardIds,
@@ -100,7 +104,11 @@ export class PlayerSection extends Component {
       const playerDeckIds = this.props.playerDeck.map(deckCard => {
         return deckCard.id;
       });
-      const discardPile = [...boughtCardIds, ...handCardIds, ...activatedCardsIds];
+      const discardPile = [
+        ...boughtCardIds,
+        ...handCardIds,
+        ...activatedCardsIds
+      ];
       this.props.discardCards(discardPile);
       this.props.endTurn();
       this.endTurn(boughtCardIds, discardPile, playerDeckIds);
@@ -146,17 +154,17 @@ export class PlayerSection extends Component {
   render() {
     const gameIdNotifier =
       this.props.tableCards.length === 0 ? (
-        <p className='game-id'>GAME ID: {this.props.gameID}</p>
+        <p className="game-id">GAME ID: {this.props.gameID}</p>
       ) : null;
 
     return (
-      <section className='PlayerSection'>
+      <section className="PlayerSection">
         {gameIdNotifier}
         <ActivatedCards />
         <PlayerDeck />
         <PlayerHand />
         <DiscardPile />
-        <button className='end-turn' onClick={this.cleanUp}>
+        <button className="end-turn" onClick={this.cleanUp}>
           End Turn
         </button>
       </section>
@@ -184,7 +192,9 @@ export const mapDispatchToProps = dispatch => ({
     dispatch(actions.updatePlayerCards(deck, hand, discardPile)),
   updateTableCards: cards => dispatch(actions.updateTableCards(cards)),
   applyActionValues: (spendingPower, buyingPower, actionsProvided) =>
-    dispatch(actions.applyActionValues(spendingPower, buyingPower, actionsProvided)),
+    dispatch(
+      actions.applyActionValues(spendingPower, buyingPower, actionsProvided)
+    ),
   discardCards: cards => dispatch(actions.discardCards(cards))
 });
 
