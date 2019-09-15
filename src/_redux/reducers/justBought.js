53ad66042f4e94754af07cf605abcab7aa4c9257
Turing School <kratok08@gmail.com>
2019-09-14 22:52:28 -0600

    Add clear_card action.type

--- src/_redux/reducers/justBought.js
+++ src/_redux/reducers/justBought.js
@@ -2,6 +2,8 @@ export const justBought = (state = "", action) => {
   switch (action.type) {
     case "BOUGHT_CARD":
       return action.cardName;
+    case "CLEAR_CARD":
+      return "";
     default:
       return state;
   }
